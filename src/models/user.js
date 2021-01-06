45c3cca2aa430a6c20a282ceef2eabe7801aa400
idcunyu <695035427@qq.com>
2021-01-06 18:18:44 +0800

    feat: add eslint and check

--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,20 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback } from 'react';
+
 export default function useAuthModel() {
-  const [user, setUser] = useState(null)
-  const signin = useCallback((account, password) => {
+  const [user, setUser] = useState(null);
+  const signin = useCallback(() => {
     // signin implementation
     // setUser(user from signin API)
-    setUser('set1')
-  }, [])
+    setUser('set1');
+  }, []);
   const signout = useCallback(() => {
     // signout implementation
     // setUser(null)
-    setUser(null)
-  }, [])
+    setUser(null);
+  }, []);
   return {
     user,
     signin,
     signout,
-  }
+  };
 }
